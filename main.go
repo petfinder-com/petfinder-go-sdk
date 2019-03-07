45cd091847c4c5bcbea1b620e3047bb3f5bbf3a4
Tim Johnson <timothy.johnson2@purina.nestle.com>
2019-03-07 12:55:46 -0500

    add comments

--- main.go
+++ main.go
@@ -9,17 +9,26 @@ import (
 
 var client *http.Client
 
+//Main() is used as an example for accessing petfinder api
 func main() {
+
+	//Pull Client ID key and Client Secret Key from environment variables
 	clientID := os.Getenv("PF_CLIENT_ID")
 	clientSecret := os.Getenv("PF_CLIENT_SECRET")
 
+	//Create pfclient Object
 	pfclient, err := pfapi.NewClient(clientID, clientSecret)
 	if err != nil {
 		fmt.Println("Could not create client")
 	}
 
+	//Create variable based on AnimalType struct
 	var types []pfapi.AnimalType
+
+	//Retreive all animal types, put into struct
 	types, _ = pfclient.GetAllTypes()
+
+	//Interate through animal types using struct data
 	for _, t := range types {
 		fmt.Println("Name: ", t.Name)
 		fmt.Println("Colors: ", t.Colors)
