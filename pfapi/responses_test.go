8416c35dab3c498831bd3e2313faea1150c877ac
Nolan Davidson <nolan.davidson@purina.nestle.com>
2019-03-08 08:53:53 -0500

    adding single type tests

--- pfapi/responses_test.go
+++ pfapi/responses_test.go
@@ -81,6 +81,51 @@ var typesJson = []byte(`
 }
 `)
 
+var singleTypeJson = []byte(`
+{
+    "type": {
+        "name": "Dog",
+        "coats": [
+            "Hairless",
+            "Short",
+            "Medium",
+            "Long",
+            "Wire",
+            "Curly"
+        ],
+        "colors": [
+            "Apricot / Beige",
+            "Bicolor",
+            "Black",
+            "Brindle",
+            "Brown / Chocolate",
+            "Golden",
+            "Gray / Blue / Silver",
+            "Harlequin",
+            "Merle (Blue)",
+            "Merle (Red)",
+            "Red / Chestnut / Orange",
+            "Sable",
+            "Tricolor (Brown, Black, & White)",
+            "White / Cream",
+            "Yellow / Tan / Blond / Fawn"
+        ],
+        "genders": [
+            "Male",
+            "Female"
+        ],
+        "_links": {
+            "self": {
+                "href": "/v2/types/dog"
+            },
+            "breeds": {
+                "href": "/v2/types/dog/breeds"
+            }
+        }
+    }
+}
+`)
+
 func TestAnimalTypesDecode(t *testing.T) {
 	var animalTypes []AnimalType
 	var message interface{}
@@ -132,5 +177,38 @@ func TestAnimalTypesDecode(t *testing.T) {
 	bird := animalTypes[1]
 	if bird.Name != "Bird" {
 		t.Errorf("Second animal type should be Bird, got %v", bird.Name)
+	}
+}
+
+func TestAnimalTypeDecode(t *testing.T) {
+	var animalType AnimalType
+	var message interface{}
+	err := json.Unmarshal(singleTypeJson, &message)
+	if err != nil {
+		t.Errorf("Could not unmarshal JSON: %v", err)
+	}
+	messageMap := message.(map[string]interface{})
+	typesMap := messageMap["type"].(interface{})
+
+	err = mapstructure.Decode(typesMap, &animalType)
+
+	if animalType.Name != "Dog" {
+		t.Errorf("Animal type should have been Dog, got %v", animalType.Name)
+	}
+
+	if len(animalType.Colors) != 15 {
+		t.Errorf("Animal type should have had 15 colors, got %v", len(animalType.Colors))
+	}
+
+	if len(animalType.Coats) != 6 {
+		t.Errorf("Animal type should have had 6 coats, got %v", len(animalType.Coats))
+	}
+
+	if len(animalType.Genders) != 2 {
+		t.Errorf("Animal type should have had 2 genders, got %v", len(animalType.Genders))
+	}
+
+	if animalType.Links.Self.Href != "/v2/types/dog" {
+		t.Errorf("Animal type should have a self link of /v2/types/dog, got %v", animalType.Links.Self.Href)
 	}
 }
