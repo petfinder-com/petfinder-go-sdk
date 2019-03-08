3f4ded8d12544a463f3c24934ed439fec1abae21
Tim Johnson <timothy.johnson2@purina.nestle.com>
2019-03-08 13:47:42 -0500

    Add animal types

--- pfapi/pftypes.go
+++ pfapi/pftypes.go
@@ -5,14 +5,102 @@ type AnimalType struct {
 	Coats   []string
 	Genders []string
 	Colors  []string
-	Links   Links `mapstructure:"_links"`
+	Links   TypeLinks `mapstructure:"_links"`
 }
 
-type Links struct {
+type TypeLinks struct {
 	Self   Link
 	Breeds Link
 }
 
 type Link struct {
 	Href string
+}
+
+type AnimalResponse struct {
+	Animals        []Animal
+	PaginationData Pagination
+}
+
+type Animal struct {
+	ID             string
+	OrganizationID string
+	URL            string
+	Type           string
+	Species        string
+	Breeds         Breeds
+	Colors         Colors
+	Age            string
+	Gender         string
+	Size           string
+	Coat           string
+	Name           string
+	Description    string
+	Photos         []Photo
+	Status         string
+	Attributes     Attribute
+	Environment    Environment
+	tags           []string
+	contact        Contact
+}
+
+type Breeds struct {
+	Primary   string
+	Secondary string
+	Mixed     string
+	Unknown   string
+}
+
+type Colors struct {
+	Primary   string
+	Secondary string
+	Tertiary  string
+}
+
+type Photo struct {
+	Small  string
+	Medium string
+	Large  string
+	Full   string
+}
+
+type Attribute struct {
+	SpayedNeutered bool
+	HouseTrainied  bool
+	Declawed       bool
+	SpecialNeeds   bool
+	ShotsCurrent   bool
+}
+
+type Environment struct {
+	Children bool
+	Dogs     bool
+	cats     bool
+}
+type Contact struct {
+	Email   string
+	Phone   string
+	Address Address
+}
+
+type Address struct {
+	Address1 string
+	Address2 string
+	City     string
+	State    string
+	PostCode string
+	Country  string
+}
+
+type Pagination struct {
+	CountPerPage int
+	TotalCount   int
+	CurrentPage  int
+	TotalPages   int
+}
+
+type AnimalLinks struct {
+	Self         Link
+	Type         Link
+	Organization Link
 }
